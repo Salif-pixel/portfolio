e162e78b4251d089bf28e0cd2d4c37f5542da3e7
Salif Biaye <salifbiaye@esp.sn>
2024-07-03 00:43:55 +0000

    first

--- src/App.tsx
+++ src/App.tsx
@@ -168,11 +168,16 @@ function App() {
                             className={work ? `h-1 mt-3  rounded-lg bg-white` : `h-1 mt-3 hidden  rounded-lg bg-white`}></motion.div>
             </div>
 
-            <svg onClick={handlePlayPauseMusic} xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" strokeWidth="1.5"
-                 stroke="currentColor" className={`cursor-pointer size-6`}>
-                <path strokeLinecap="round" strokeLinejoin="round"
-                      d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z"/>
-            </svg>
+            {isPlaying?<svg onClick={handlePlayPauseMusic}  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className={`cursor-pointer size-6`}>
+                <path fillRule="evenodd"
+                      d="M19.952 1.651a.75.75 0 0 1 .298.599V16.303a3 3 0 0 1-2.176 2.884l-1.32.377a2.553 2.553 0 1 1-1.403-4.909l2.311-.66a1.5 1.5 0 0 0 1.088-1.442V6.994l-9 2.572v9.737a3 3 0 0 1-2.176 2.884l-1.32.377a2.553 2.553 0 1 1-1.402-4.909l2.31-.66a1.5 1.5 0 0 0 1.088-1.442V5.25a.75.75 0 0 1 .544-.721l10.5-3a.75.75 0 0 1 .658.122Z"
+                      clipRule="evenodd"/>
+            </svg>:<svg onClick={handlePlayPauseMusic} xmlns="http://www.w3.org/2000/svg" fill="black"
+                                                       viewBox="0 0 24 24" strokeWidth="1.5"
+                                                       stroke="currentColor" className={`cursor-pointer size-6`}>
+            <path strokeLinecap="round" strokeLinejoin="round"
+                  d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z"/>
+        </svg>}
             <audio ref={audioRef} src={music}/>
         </div>
 
